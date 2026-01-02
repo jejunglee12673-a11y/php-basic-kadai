7549d71a4f3cd2c071524f49f2bb1e0a84ed34ac
jejunglee12673-a11y <jejunglee12673@gmail.com>
2026-01-02 13:54:03 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UFF-8">
+  <title>PHP_7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $personal_date = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($personal_date);
+
+    ?>
+  </p>
+</body>
+
+</html>
